d3ef0725661bc9df6c746d47f66d9453fa0c2c53
colindominic <32608479+colindominic@users.noreply.github.com>
2018-03-03 19:20:11 -0800

    step 40 rev 2

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,12 +4,11 @@ import Track from '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
-    console.log(this.props.tracks);
     return (
       <div className="TrackList">
-        this.props.tracks.map(track => {
-          return <Track key={this.props.track.id} name={this.props.track.name} artist={this.props.track.artist} album={this.props.track.album} />
-        });
+        {this.props.tracks.map(track => {
+           return <Track key={track.id} name={track.name} artist={track.artist} album={track.album} />
+        })}
       </div>
     );
   }
