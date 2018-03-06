718c05b5beb48cf86df2788344ece22963a3cc8c
colindominic <32608479+colindominic@users.noreply.github.com>
2018-03-05 21:01:12 -0800

    step 73 complete

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,12 +5,16 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      term: ''
+    };
+
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search() {
-    this.props.onSearch = this.state.term;
+    this.props.onSearch(this.state.term);
   }
 
   handleTermChange(event) {
@@ -20,7 +24,7 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
         <a>SEARCH</a>
       </div>
     );
