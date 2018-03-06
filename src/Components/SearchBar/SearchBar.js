f7223027762ca054c117cb9c1d96e4365277dbaa
colindominic <32608479+colindominic@users.noreply.github.com>
2018-03-05 20:38:01 -0800

    step 69

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search() {
+    this.props.onSearch = this.state.term;
+  }
+
+  handleTermChange(event) {
+    this.state.term = event.target.value;
+  }
+
   render() {
     return (
       <div className="SearchBar">
